9b4caa98e2bfee00485e43cae13304872b9afb25
will <willmiller629@gmail.com>
2021-04-26 20:39:46 -0400

    insert into linked list

---
+++ src/main/java/LinkedList/Main.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       LinkedList list = new LinkedList();
+       list.insert(10);
+       list.insert(23);
+       list.insert(45);
+
+       list.showValues();
+
+    }
+}
